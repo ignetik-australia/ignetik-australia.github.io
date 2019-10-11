275d4a1ff2edb4e715b20dc1bdf35d2f1c94bde4
Oliver Ignetik <olli@ozemail.com.au>
2019-10-11 15:12:49 +1100

    added animation to menu item

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    let clicks = 0; 
+
     // Navbar fading
     $('#Navbar a').hover(function () {
         // over
@@ -6,5 +8,16 @@ $(document).ready(function () {
     }, function () {
         // out
         $(this).css("opacity", 1)
+    });
+
+    $(".hamburger").click(function (e) { 
+        e.preventDefault();
+        if (clicks%2==0){
+            $(this).addClass("is-active");    
+        }
+        if (clicks%2!=0){
+            $(this).removeClass("is-active");
+        }
+        ++clicks;
     });
 });
