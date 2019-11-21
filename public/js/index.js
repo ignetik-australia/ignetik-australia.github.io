fee0b54becc1d50cd8a895e41fb631262ce9fc88
Oliver Ignetik <olli@ozemail.com.au>
2019-11-21 18:40:39 +1100

    refresh preloader bug fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
-    $(window).on("load", () => {
-        // listen for load of window
-        setTimeout(function () {
-            $(".loader-wrapper").addClass('zero-opacity')
-        }, 1500);
-        setTimeout(function () {
-            $(".loader-wrapper").addClass('hidden');
-        }, 2500);
-    })
+    // listen for load of window
+    setTimeout(function () {
+        $(".loader-wrapper").addClass('zero-opacity')
+    }, 1500);
+    setTimeout(function () {
+        $(".loader-wrapper").addClass('hidden');
+    }, 2500);
 
     let clicks = 0;
 
